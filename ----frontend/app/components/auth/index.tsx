27914dce7176377b098c6c010a8c2b4381ef8254
Lex Trofimov <daslef93@gmail.com>
2025-07-04 10:22:52 +0300

    WIP: auth

--- ----frontend/app/components/auth/index.tsx
+++ ----frontend/app/components/auth/index.tsx
@@ -8,12 +8,6 @@ export function AuthWrapper({ children }: PropsWithChildren) {
     return (
         <article className={classes.auth_wrapper}>
             <div className={classes.auth}>
-                <section className={classes.tabs}>
-                    <Link to="/login" className={clsx(classes.link, classes.link_login)}>Войти</Link>
-                    <Link to="/signup" className={clsx(classes.link, classes.link_signup)}>Зарегистрироваться</Link>
-                    {/* <h3 id="login"><a href="/login">Войти</a></h3>
-                    <h3 id="register"><a href="./registration.html"></a>Зарегистрироваться</h3> */}
-                </section>
                 {children}
             </div>
         </article>
@@ -25,29 +19,35 @@ export function LoginForm() {
     const errors = fetcher.data?.errors;
 
     return (
-        <fetcher.Form method="post" className={classes.form}>
-            <section className={classes.input_group}>
-                <input type="email" id="email" name="email" placeholder="Ваш E-mail" required className={classes.input} />
-                {errors?.email ? <em>{errors.email}</em> : null}
+        <>
+            <section className={classes.tabs}>
+                <Link to="/login" className={clsx(classes.link, classes.active)}>Войти</Link>
+                <Link to="/signup" className={clsx(classes.link)}>Зарегистрироваться</Link>
             </section>
+            <fetcher.Form method="post" className={classes.form}>
+                <section className={classes.input_group}>
+                    <input type="email" id="email" name="email" placeholder="Ваш E-mail" required className={classes.input} />
+                    {errors?.email ? <em>{errors.email}</em> : null}
+                </section>
 
-            <section className={classes.input_group}>
-                <input type="password" id="password" name="password" placeholder="Пароль" required className={classes.input} />
-                <span className={classes.toggle_password}></span>
-                {errors?.password ? (
-                    <em>{errors.password}</em>
-                ) : null}
-            </section>
+                <section className={classes.input_group}>
+                    <input type="password" id="password" name="password" placeholder="Пароль" required className={classes.input} />
+                    <span className={classes.toggle_password}></span>
+                    {errors?.password ? (
+                        <em>{errors.password}</em>
+                    ) : null}
+                </section>
 
-            <section className={classes.checkbox_block}>
-                <input type="checkbox" id="checkbox" name="checkbox" required />
-                <label className={classes.checkbox_label} htmlFor="checkbox">Согласен с Правилами и Политикой конфиденциальности</label>
-            </section>
+                <section className={classes.checkbox_block}>
+                    <input type="checkbox" id="checkbox" name="checkbox" required />
+                    <label className={classes.checkbox_label} htmlFor="checkbox">Согласен с Правилами и Политикой конфиденциальности</label>
+                </section>
 
-            <button type="submit" id="button" value="Продолжить" className={classes.button}>Продолжить</button>
+                <button type="submit" id="button" value="Продолжить" className={classes.button}>Продолжить</button>
 
-            <Link to="/remind-password">Забыли пароль?</Link>
-        </fetcher.Form>
+                <Link to="/remind-password">Забыли пароль?</Link>
+            </fetcher.Form>
+        </>
     );
 }
 
@@ -56,34 +56,40 @@ export function SignupForm() {
     const errors = fetcher.data?.errors;
 
     return (
-        <fetcher.Form method="post" className={classes.form}>
-            <section className={classes.input_group}>
-                <input type="email" id="email" name="email" placeholder="Ваш E-mail" required className={classes.input} />
-                {errors?.email ? <em>{errors.email}</em> : null}
+        <>
+            <section className={classes.tabs}>
+                <Link to="/login" className={clsx(classes.link)}>Войти</Link>
+                <Link to="/signup" className={clsx(classes.link, classes.active)}>Зарегистрироваться</Link>
             </section>
+            <fetcher.Form method="post" className={classes.form}>
+                <section className={classes.input_group}>
+                    <input type="email" id="email" name="email" placeholder="Ваш E-mail" required className={classes.input} />
+                    {errors?.email ? <em>{errors.email}</em> : null}
+                </section>
 
-            <section className={classes.input_group}>
-                <input type="password" id="password" name="password" placeholder="Пароль" required className={classes.input} />
-                <span className={classes.toggle_password}></span>
-                {errors?.password ? (
-                    <em>{errors.password}</em>
-                ) : null}
-            </section>
+                <section className={classes.input_group}>
+                    <input type="password" id="password" name="password" placeholder="Пароль" required className={classes.input} />
+                    <span className={classes.toggle_password}></span>
+                    {errors?.password ? (
+                        <em>{errors.password}</em>
+                    ) : null}
+                </section>
 
-            <section className={classes.input_group}>
-                <input type="password" id="password-check" name="confirm_password" placeholder="Подтверждение пароля" required className={classes.input} />
-                <span className={classes.toggle_password}></span>
-                {errors?.confirm_password ? (
-                    <em>{errors.confirm_password}</em>
-                ) : null}
-            </section>
+                <section className={classes.input_group}>
+                    <input type="password" id="password-check" name="confirm_password" placeholder="Подтверждение пароля" required className={classes.input} />
+                    <span className={classes.toggle_password}></span>
+                    {errors?.confirm_password ? (
+                        <em>{errors.confirm_password}</em>
+                    ) : null}
+                </section>
 
-            <section className={classes.checkbox_block}>
-                <input type="checkbox" id="checkbox" name="checkbox" required />
-                <label className={classes.checkbox_label} htmlFor="checkbox">Согласен с Правилами и Политикой конфиденциальности</label>
-            </section>
+                <section className={classes.checkbox_block}>
+                    <input type="checkbox" id="checkbox" name="checkbox" required />
+                    <label className={classes.checkbox_label} htmlFor="checkbox">Согласен с Правилами и Политикой конфиденциальности</label>
+                </section>
 
-            <button type="submit" id="button" value="Продолжить" className={classes.button}>Продолжить</button>
-        </fetcher.Form>
+                <button type="submit" id="button" value="Продолжить" className={classes.button}>Продолжить</button>
+            </fetcher.Form>
+        </>
     );
 }
